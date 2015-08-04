f7771a442dbd5deef71f59a2cde2b36029a8ed09
Alexander <nefedov.alexander91@yandex.ru>
2015-08-04 20:40:30 +0500

    Replaced function-based views with class-based views
    Modified files:
    	polls/templates/polls/detail.html
    	polls/templates/polls/results.html
    	polls/urls.py
    	polls/views.py

--- busy_badger/polls/urls.py
+++ busy_badger/polls/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url( r"^$", views.index, name="index" ),
-    url( r"^(?P<question_id>[0-9]+)/$", views.details, name="details" ),
-    url( r"^(?P<question_id>[0-9]+)/res/$", views.results, name="results" ),
+    url( r"^$", views.XIndexView.as_view(), name="index" ),
+    url( r"^(?P<pk>[0-9]+)/$", views.XDetailView.as_view(), name="details" ),
+    url( r"^(?P<pk>[0-9]+)/res/$", views.XResultsView.as_view(), name="results" ),
     url( r"^(?P<question_id>[0-9]+)/vote/$", views.vote, name="vote" ),
 ]
--- busy_badger/polls/views.py
+++ busy_badger/polls/views.py
@@ -1,21 +1,29 @@
 
-# from django.http import HttpResponse
-# from django.template import RequestContext, loader
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.shortcuts           import render, get_object_or_404
+from django.http                import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers   import reverse
+from django.views               import generic
 
-from .models import XQuestion, XChoice
+from .models                    import XQuestion, XChoice
 
 
-def index( request ):
-    latest_question_list = XQuestion.objects.order_by( "-pub_date" )[ :10 ]
-    cont = { "latest_question_list": latest_question_list }
-    return render( request, "polls/index.html", cont )
+class XIndexView( generic.ListView ):
+    template_name       = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def details( request, question_id ):
-    question = get_object_or_404( XQuestion, pk=question_id )
-    return render( request, "polls/detail.html", {"question": question} )
+    def get_queryset( self ):
+        return XQuestion.objects.order_by( "-pub_date" )[ :10 ]
+
+
+class XDetailView( generic.DetailView ):
+    model           = XQuestion
+    template_name   = "polls/detail.html"
+
+
+class XResultsView( generic.DetailView ):
+    model           = XQuestion
+    template_name   = "polls/results.html"
+
 
 def vote( request, question_id ):
     p = get_object_or_404( XQuestion, pk=question_id )
@@ -31,7 +39,3 @@ def vote( request, question_id ):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect( reverse( "polls:results", args=( p.id, ) ) )
-
-def results( request, question_id ):
-    question = get_object_or_404( XQuestion, pk=question_id )
-    return render( request, "polls/results.html", {"question": question } )
