3aea046e134f9f2b11c1ea17655b3a0f3a6ff0ef
Alexander <nefedov.alexander91@yandex.ru>
2015-08-04 17:35:25 +0500

    Played around with admin interface

--- busy_badger/polls/admin.py
+++ busy_badger/polls/admin.py
@@ -1,3 +1,17 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import XQuestion, XChoice
+
+class XQuestionAdmin( admin.ModelAdmin ):
+    fields = [ "question_text" ]
+
+class XChoiceAdmin( admin.ModelAdmin ):
+    fieldsets = [
+        (
+            "General", { "fields": ["choice_text"] }
+        )
+    ]
+
+admin.site.register( XQuestion, XQuestionAdmin )
+admin.site.register( XChoice, XChoiceAdmin )
