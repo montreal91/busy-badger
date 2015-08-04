651dd964fc6f02de086eb884485bd668aed00971
Alexander <nefedov.alexander91@yandex.ru>
2015-08-04 20:13:35 +0500

    Implemented actual voting
    Modified files:
    	polls/templates/polls/detail.html
    	polls/urls.py
    	polls/views.py
    
    New file:
    	polls/templates/polls/results.html

--- busy_badger/polls/urls.py
+++ busy_badger/polls/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     url( r"^$", views.index, name="index" ),
-    url( r"^(?P<question_id>[0-9]+)$", views.details, name="details" ),
+    url( r"^(?P<question_id>[0-9]+)/$", views.details, name="details" ),
+    url( r"^(?P<question_id>[0-9]+)/res/$", views.results, name="results" ),
+    url( r"^(?P<question_id>[0-9]+)/vote/$", views.vote, name="vote" ),
 ]
--- busy_badger/polls/views.py
+++ busy_badger/polls/views.py
@@ -2,15 +2,36 @@
 # from django.http import HttpResponse
 # from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from .models import XQuestion
+from .models import XQuestion, XChoice
 
 
 def index( request ):
     latest_question_list = XQuestion.objects.order_by( "-pub_date" )[ :10 ]
-    context = { "latest_question_list": latest_question_list }
-    return render( request, "polls/index.html", context )
+    cont = { "latest_question_list": latest_question_list }
+    return render( request, "polls/index.html", cont )
 
 def details( request, question_id ):
-    question = get_object_or_404( XQuestion, pk=question_id)
-    return render( request, "polls/detail.html", {"question": question})
+    question = get_object_or_404( XQuestion, pk=question_id )
+    return render( request, "polls/detail.html", {"question": question} )
+
+def vote( request, question_id ):
+    p = get_object_or_404( XQuestion, pk=question_id )
+    try:
+        selected_choice = p.xchoice_set.get( pk=request.POST[ "choice" ] )
+    except ( KeyError, XChoice.DoesNotExist ):
+        cont = {
+            "question": p,
+            "error_message": "You didn't select a choice.",
+        }
+        return render( request, "polls/detail.html", cont )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect( reverse( "polls:results", args=( p.id, ) ) )
+
+def results( request, question_id ):
+    question = get_object_or_404( XQuestion, pk=question_id )
+    return render( request, "polls/results.html", {"question": question } )
