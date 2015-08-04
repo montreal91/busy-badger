1e2cc442e90a594f8c2de6eaffd8da50bd028c15
Alexander <nefedov.alexander91@yandex.ru>
2015-08-04 18:41:40 +0500

    Required views are implemented
    -index view in polls/views.py
    -details view in polls/views.py
    -required url dispatching is added

--- busy_badger/polls/views.py
+++ busy_badger/polls/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
-# Create your views here.
+# from django.http import HttpResponse
+# from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
+
+from .models import XQuestion
+
+
+def index( request ):
+    latest_question_list = XQuestion.objects.order_by( "-pub_date" )[ :10 ]
+    context = { "latest_question_list": latest_question_list }
+    return render( request, "polls/index.html", context )
+
+def details( request, question_id ):
+    question = get_object_or_404( XQuestion, pk=question_id)
+    return render( request, "polls/detail.html", {"question": question})
